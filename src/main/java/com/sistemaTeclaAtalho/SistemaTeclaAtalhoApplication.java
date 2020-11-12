eb0f9a070c817ce1725c13e2e30c2c123ecff4b3
Alex Rocha <alexrochadealbuquerque@gmail.com>
2020-11-12 10:20:27 -0300

    Projeto criado

---
+++ src/main/java/com/sistemaTeclaAtalho/SistemaTeclaAtalhoApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaTeclaAtalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTeclaAtalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTeclaAtalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaTeclaAtalho/SistemaTeclaAtalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaTeclaAtalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTeclaAtalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
